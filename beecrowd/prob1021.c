1e219f18b6c2369f922f797d1e04a63a8e8c5069
S. M. Redwan <smredwan.info@gmail.com>
2022-06-14 17:42:11 +0600

    Commited after solving bank note and coin

--- beecrowd/prob1021.c
+++ beecrowd/prob1021.c
@@ -10,59 +10,58 @@ int main() {
     //taking input form user
     scanf("%lf", &inpt_amount);
 
-    //preprocessing data
-    for_note = inpt_amount;
-    for_coin = (inpt_amount - for_note) * 100;
-    
-    
-    //data processing start
-    //notes
-    note100 = for_note / 100;
-    for_note %= 100;
-    note50 = for_note / 50;
-    for_note %= 50;
-    note20 = for_note / 20;
-    for_note %= 20;
-    note10 = for_note / 10;
-    for_note %= 10;
-    note5 = for_note / 5;
-    for_note %= 5;
-    note2 = for_note / 2;
-    for_note %= 2;
-
-    //coin
-    coin100 = for_coin / 100;
-    for_coin %= 100;
-    coin50 = for_coin / 50;
-    for_coin %= 50;
-    coin25 = for_coin / 25;
-    for_coin %= 25;
-    coin10 = for_coin / 10;
-    for_coin %= 10;
-    coin5 = for_coin / 5;
-    for_coin %= 5;
-    coin5 = for_coin / 5;
-    for_coin %= 5;
-    coin1 = for_coin;
-
+    //cheaking data is in rang or not?
+    if(inpt_amount > 0 && inpt_amount < 1000000.00) {
+        //preprocessing data
+        for_note = (int)inpt_amount;
+        for_coin = (inpt_amount - for_note) * 100;    
+        
+        //data processing start
+        //notes
+        note100 = for_note / 100;
+        for_note %= 100;
+        note50 = for_note / 50;
+        for_note %= 50;
+        note20 = for_note / 20;
+        for_note %= 20;
+        note10 = for_note / 10;
+        for_note %= 10;
+        note5 = for_note / 5;
+        for_note %= 5;
+        note2 = for_note / 2;
+        for_note %= 2;
+        //coins
+        coin100 = for_note;
+        for_coin %= 100;
+        coin50 = for_coin / 50;
+        for_coin %= 50;
+        coin25 = for_coin / 25;
+        for_coin %= 25;
+        coin10 = for_coin / 10;
+        for_coin %= 10;
+        coin5 = for_coin / 5;
+        for_coin %= 5;
+        coin1 = for_coin;
+        //data processing end
+    }
+        
     //showing outputs:
+    //notes
     printf("NOTAS:\n");
     printf("%d nota(s) de R$ 100.00\n", note100);
     printf("%d nota(s) de R$ 50.00\n", note50);
     printf("%d nota(s) de R$ 20.00\n", note20);
     printf("%d nota(s) de R$ 10.00\n", note10);
     printf("%d nota(s) de R$ 5.00\n", note5);
-    printf("%d nota(s) de R$ 2.00\n", note100);
-
+    printf("%d nota(s) de R$ 2.00\n", note2);
+    //coins
     printf("MOEDAS:\n");
-    printf("%d nota(s) de R$ 1.00\n", coin100);
-    printf("%d nota(s) de R$ 0.50\n", coin50);
-    printf("%d nota(s) de R$ 0.25\n", coin25);
-    printf("%d nota(s) de R$ 0.10\n", coin10);
-    printf("%d nota(s) de R$ 0.05\n", coin5);
-    printf("%d nota(s) de R$ 0.01\n", coin1);
-
-
+    printf("%d moeda(s) de R$ 1.00\n", coin100);
+    printf("%d moeda(s) de R$ 0.50\n", coin50);
+    printf("%d moeda(s) de R$ 0.25\n", coin25);
+    printf("%d moeda(s) de R$ 0.10\n", coin10);
+    printf("%d moeda(s) de R$ 0.05\n", coin5);
+    printf("%d moeda(s) de R$ 0.01\n", coin1);
 
     return 0;
 }

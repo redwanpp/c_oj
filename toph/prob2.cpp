40c3a98174f9c43dfa4edf7d818b8791b563e750
S. M. Redwan <smredwan.info@gmail.com>
2022-07-01 21:58:42 +0600

    Commited after solving first 2 problem in toph

---
+++ toph/prob2.cpp
@@ -0,0 +1,22 @@
+//Add them up
+
+#include <iostream>
+#define endl "\n"
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int num1, num2, sum;
+
+    cin >> num1 >> num2;
+
+    sum = num1 + num2;
+
+    cout << sum;
+
+    return 0;
+}
